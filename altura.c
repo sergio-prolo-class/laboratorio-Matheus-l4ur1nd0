df849734338698bd42dad7cc53cf337bb7a97347
Matheus-l4ur1nd0 <matheus.l09@aluno.ifsc.edu.br>
2025-05-23 10:47:27 -0300

    Update altura.c

--- altura.c
+++ altura.c
@@ -1,1 +1,22 @@
+#include <stdio.h>
 
+#define 
+
+int main()
+{
+    int altura_pes;
+    int altura_plg;
+    float calculo_cm
+
+    printf("Digite a altura (pés): ");
+    scanf("%d", &altura_pes);
+
+    printf("Digite a altura (plg): ");
+    scanf("%d", &altura_plg);
+
+    calculo_cm = altura_pes * 12 + altura_plg;
+
+    printf("Sua altura em (m/cm) é: %f \n", calculo_cm);
+
+
+}
