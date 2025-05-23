fff05d83702cb458bec21458d953fc817f179464
Matheus-l4ur1nd0 <matheus.l09@aluno.ifsc.edu.br>
2025-05-23 13:48:37 -0300

    Update caixas.c

--- caixas.c
+++ caixas.c
@@ -15,6 +15,10 @@ int main()
     scanf("%f", &largura);
 
     area_superficie = 2 * (altura * largura + altura * profundidade + largura * profundidade);
+    volume = altura * largura * profundidade;
 
+    printf("\nÁrea da superfície: %.2f m²\n", area_superficie);
+    printf("Volume da caixa: %.2f m³\n", volume);
+    
     return (0);
 }
